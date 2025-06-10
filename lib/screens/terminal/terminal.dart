6d979a382770ecb0737ee631c1e52de52980f79d
ahmadexe <muahmad710@gmail.com>
2025-06-10 09:23:13 +0500

    refac: moved url service in a specific folder

--- lib/screens/terminal/terminal.dart
+++ lib/screens/terminal/terminal.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_linkify/flutter_linkify.dart';
 import 'package:provider/provider.dart';
 import 'package:termolio/configs/configs.dart';
-import 'package:termolio/services/url_service.dart';
+import 'package:termolio/services/url/url_service.dart';
 import 'package:termolio/widgets/core/screen.dart';
 
 part '_state.dart';
