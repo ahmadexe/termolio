c0850f269ee9cbb5f40cc51ba8c296368a5364e0
ahmadexe <muahmad710@gmail.com>
2025-06-12 03:07:31 +0500

    feat: deployment setup

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: TerminalScreen());
+    return MaterialApp(
+      home: TerminalScreen(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
